f2a7a2db4bed3dbd508485e6909947ad25a41d88
suwei1979 <suwei1979@139.com>
2015-11-27 16:27:08 +0800

    test

--- learn-parent/learn-java/src/main/java/org/suw/learn/java/generics/ResovleGenericsType.java
+++ learn-parent/learn-java/src/main/java/org/suw/learn/java/generics/ResovleGenericsType.java
@@ -5,18 +5,20 @@ package org.suw.learn.java.generics;
 
 import javax.xml.bind.annotation.XmlInlineBinaryData;
 
+import lombok.Data;
+
 /**
  * @author suwei
  *
  */
-
+@Data
 public class ResovleGenericsType<E> {
     E genericType;
     /**
      * 
      */
-    public ResovleGenericsType() {
-        // TODO Auto-generated constructor stub
+    public ResovleGenericsType(E genericTypeParm) {
+        genericType = genericTypeParm
     }
 
     public static void main(String[] args) {
