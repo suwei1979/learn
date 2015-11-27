10cc93ccec74dbc52080088bad6176c8b4f50c27
suwei1979 <suwei1979@139.com>
2015-11-27 16:25:41 +0800

    解析泛型类型测试

---
+++ learn-parent/learn-java/src/main/java/org/suw/learn/java/generics/ResovleGenericsType.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2015 Baidu, Inc. All Rights Reserved.
+ */
+package org.suw.learn.java.generics;
+
+import javax.xml.bind.annotation.XmlInlineBinaryData;
+
+/**
+ * @author suwei
+ *
+ */
+
+public class ResovleGenericsType<E> {
+    E genericType;
+    /**
+     * 
+     */
+    public ResovleGenericsType() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
