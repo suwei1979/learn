97b33ae6af540ecafd12f60752f2a5cf1812b54a
suwei1979 <suwei1979@139.com>
2016-02-14 17:10:25 +0800

    the library from domainlanguage.com

---
+++ learn-parent/learn-domain/src/main/java/com/baidu/rigel/mdm/api/quanjing/AccountService.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.rigel.mdm.api.quanjing;
+
+/**
+ * @author suwei
+ *
+ */
+public interface AccountService {
+
+}
