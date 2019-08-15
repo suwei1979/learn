a2f49376df7c306db3d769eb2d41fef9a704194c
suwei <suwei1979@139.com>
2019-08-15 14:47:04 +0800

    continuous refactories

--- learn-parent/lab-spring/lab-spring-beans/src/main/java/org/suw/lab/spring/beans/config/LearnSpringBeanConfiguration.java
+++ learn-parent/lab-spring/lab-spring-beans/src/main/java/org/suw/lab/spring/beans/config/LearnSpringBeanConfiguration.java
@@ -5,7 +5,6 @@ package org.suw.lab.spring.beans.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD:learn-parent/lab-spring/lab-spring-beans/src/main/java/org/suw/lab/spring/beans/config/LearnSpringBeanConfiguration.java
 import org.suw.lab.spring.beans.generics.GenericService;
 import org.suw.lab.spring.beans.generics.GenericServiceImpl;
 import org.suw.lab.spring.beans.generics.GenericServiceInjector;
@@ -13,15 +12,6 @@ import org.suw.lab.spring.beans.model.Item;
 import org.suw.lab.spring.beans.model.ItemFactory;
 import org.suw.lab.spring.beans.model.Order;
 import org.suw.lab.spring.beans.model.OrderFactory;
-=======
-import org.suw.learn.spring.beans.generics.GenericService;
-import org.suw.learn.spring.beans.generics.GenericServiceImpl;
-import org.suw.learn.spring.beans.generics.GenericServiceInjector;
-import org.suw.learn.spring.beans.model.Item;
-import org.suw.learn.spring.beans.model.ItemFactory;
-import org.suw.learn.spring.beans.model.Order;
-import org.suw.learn.spring.beans.model.OrderFactory;
->>>>>>> master:learn-parent/learn-spring/learn-spring-beans/src/main/java/org/suw/learn/spring/beans/config/LearnSpringBeanConfiguration.java
 
 /**
  * @author suwei
@@ -48,14 +38,6 @@ public class LearnSpringBeanConfiguration {
         return new GenericServiceImpl<>(Item.class);
     }
 
-<<<<<<< HEAD:learn-parent/lab-spring/lab-spring-beans/src/main/java/org/suw/lab/spring/beans/config/LearnSpringBeanConfiguration.java
-    //    @Bean
-    //    public Application application() {
-    //        return new LearnSpringBeansApplication();
-    //    }
-
-=======
->>>>>>> master:learn-parent/learn-spring/learn-spring-beans/src/main/java/org/suw/learn/spring/beans/config/LearnSpringBeanConfiguration.java
     @Bean
     GenericServiceInjector genericServiceInjector() {
         return new GenericServiceInjector();
